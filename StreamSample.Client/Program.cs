e585009d0a26279a437330c3f7b4c882f032927d
liam <liam.wang@live.com>
2020-07-06 00:18:48 +0800

    first commit

---
+++ StreamSample.Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using StreamJsonRpc;
+using System;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+namespace StreamSample.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var stream = new NamedPipeClientStream(".",
+                "StringJsonRpc",
+                PipeDirection.InOut,
+                PipeOptions.Asynchronous);
+
+            Console.WriteLine("正在连接服务器...");
+            await stream.ConnectAsync();
+            Console.WriteLine("已建立连接！");
+
+            Console.WriteLine("我是精致码农，开始向服务端问好...");
+            var jsonRpc = JsonRpc.Attach(stream);
+            var message = await jsonRpc.InvokeAsync<string>("SayHello", "精致码农");
+            Console.WriteLine($"来自服务端的响应：{message}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StreamSample.Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using StreamJsonRpc;
+using System;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+namespace StreamSample.Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int clientId = 1;
+
+            while (true)
+            {
+                var stream = new NamedPipeServerStream("StringJsonRpc",
+                   PipeDirection.InOut,
+                   NamedPipeServerStream.MaxAllowedServerInstances,
+                   PipeTransmissionMode.Byte,
+                   PipeOptions.Asynchronous);
+
+                Console.WriteLine("等待客户端连接...");
+                await stream.WaitForConnectionAsync();
+                Console.WriteLine($"已与客户端 #{clientId} 建立连接");
+
+                _ = ResponseAsync(stream, clientId);
+
+                clientId++;
+            }
+        }
+
+        static async Task ResponseAsync(NamedPipeServerStream stream, int clientId)
+        {
+            var jsonRpc = JsonRpc.Attach(stream, new GreeterServer());
+            await jsonRpc.Completion;
+            Console.WriteLine($"客户端 #{clientId} 的已断开连接");
+            jsonRpc.Dispose();
+            await stream.DisposeAsync();
+        }
+    }
+
+    public class GreeterServer
+    {
+        public string SayHello(string name)
+        {
+            Console.WriteLine($"收到【{name}】的问好，并回复了他");
+            return $"您好，{name}！";
+        }
+    }
+}
